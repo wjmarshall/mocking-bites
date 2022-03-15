a9554903a053b6b28aa50cd18e014d4c93f3b07e
William Marshall <w.marshall@elsevier.com>
2022-03-15 10:42:53 +0000

    Completed challenge

--- mocking_bites/spec/task_list_spec.rb
+++ mocking_bites/spec/task_list_spec.rb
@@ -11,5 +11,43 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq false
   end
 
+  it "can return a list of all tasks" do
+    task_list = TaskList.new
+    task_1 = double(:task)
+    task_2 = double(:task)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all).to eq([task_1, task_2])
+  end
+
+  it "returns true if all tasks are complete" do
+    task_list = TaskList.new
+    task_1 = double(:task)
+    task_2 = double(:task)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    allow(task_1).to receive(:complete?).and_return(true)
+    allow(task_2).to receive(:complete?).and_return(true)
+    expect(task_list.all_complete?).to eq(true)
+  end
+
+  it "returns true if all tasks are complete" do
+    task_list = TaskList.new
+    task_1 = double(:task, complete?: true)
+    task_2 = double(:task, complete?: true)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to eq(true)
+  end
+
+  it "returns false if all tasks are complete" do
+    task_list = TaskList.new
+    task_1 = double(:task, complete?: false)
+    task_2 = double(:task, complete?: true)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to eq(false)
+  end
+  
   # Unit test `#all` and `#all_complete?` behaviour
 end
